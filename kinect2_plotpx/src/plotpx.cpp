c1b595bf0f1e56c70fe7293e39f851f01c58eb37
Omer Rajput <omer.rajput@tuhh.de>
2016-09-30 01:10:25 +0200

    A node that allows selection of a pixel and publishes the corresponding depth value.

---
+++ kinect2_plotpx/src/plotpx.cpp
@@ -0,0 +1,491 @@
+/**
+ * Copyright 2014 University of Bremen, Institute for Artificial Intelligence
+ * Author: Thiemo Wiedemeyer <wiedemeyer@cs.uni-bremen.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <mutex>
+#include <thread>
+#include <chrono>
+
+#include <opencv2/opencv.hpp>
+
+#include <ros/ros.h>
+#include <ros/spinner.h>
+#include <std_msgs/UInt16.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <sensor_msgs/Image.h>
+
+#include <cv_bridge/cv_bridge.h>
+
+#include <image_transport/image_transport.h>
+#include <image_transport/subscriber_filter.h>
+
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/exact_time.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+#include <kinect2_bridge/kinect2_definitions.h>
+
+class Receiver
+{
+private:
+  std::mutex lock;
+
+  const std::string topicColor, topicDepth, topicPxDepth;
+  const bool useExact, useCompressed;
+
+  bool updateImage;
+  bool save;
+  bool running;
+  size_t frame;
+  const size_t queueSize;
+
+  cv::Mat color, depth;
+  cv::Mat cameraMatrixColor, cameraMatrixDepth;
+  cv::Mat lookupX, lookupY;
+
+  cv::Point pxLoc;
+
+  typedef message_filters::sync_policies::ExactTime<sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::CameraInfo, sensor_msgs::CameraInfo> ExactSyncPolicy;
+  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::CameraInfo, sensor_msgs::CameraInfo> ApproximateSyncPolicy;
+
+  ros::NodeHandle nh;
+  ros::AsyncSpinner spinner;
+  image_transport::ImageTransport it;
+  image_transport::SubscriberFilter *subImageColor, *subImageDepth;
+  message_filters::Subscriber<sensor_msgs::CameraInfo> *subCameraInfoColor, *subCameraInfoDepth;
+
+  message_filters::Synchronizer<ExactSyncPolicy> *syncExact;
+  message_filters::Synchronizer<ApproximateSyncPolicy> *syncApproximate;
+
+  ros::Publisher pxDepthPub;
+
+  std::thread imageViewerThread;
+
+  std::ostringstream oss;
+  std::vector<int> params;
+
+public:
+  Receiver(const std::string &topicColor, const std::string &topicDepth, const std::string &topicPxDepth, const bool useExact, const bool useCompressed)
+    : topicColor(topicColor), topicDepth(topicDepth), topicPxDepth(topicPxDepth), useExact(useExact), useCompressed(useCompressed),
+      updateImage(false), save(false), running(false), frame(0), queueSize(5),
+      nh("~"), spinner(0), it(nh)
+  {
+    pxLoc = cv::Point(0,0);
+    cameraMatrixColor = cv::Mat::zeros(3, 3, CV_64F);
+    cameraMatrixDepth = cv::Mat::zeros(3, 3, CV_64F);
+    params.push_back(cv::IMWRITE_JPEG_QUALITY);
+    params.push_back(100);
+    params.push_back(cv::IMWRITE_PNG_COMPRESSION);
+    params.push_back(1);
+    params.push_back(cv::IMWRITE_PNG_STRATEGY);
+    params.push_back(cv::IMWRITE_PNG_STRATEGY_RLE);
+    params.push_back(0);
+  }
+
+  ~Receiver()
+  {
+  }
+
+  void run()
+  {
+    start();
+    stop();
+  }
+
+private:
+  void start()
+  {
+    running = true;
+
+    std::string topicCameraInfoColor = topicColor.substr(0, topicColor.rfind('/')) + "/camera_info";
+    std::string topicCameraInfoDepth = topicDepth.substr(0, topicDepth.rfind('/')) + "/camera_info";
+
+    image_transport::TransportHints hints(useCompressed ? "compressed" : "raw");
+    subImageColor = new image_transport::SubscriberFilter(it, topicColor, queueSize, hints);
+    subImageDepth = new image_transport::SubscriberFilter(it, topicDepth, queueSize, hints);
+    subCameraInfoColor = new message_filters::Subscriber<sensor_msgs::CameraInfo>(nh, topicCameraInfoColor, queueSize);
+    subCameraInfoDepth = new message_filters::Subscriber<sensor_msgs::CameraInfo>(nh, topicCameraInfoDepth, queueSize);
+
+    pxDepthPub = nh.advertise<std_msgs::UInt16>(topicPxDepth, queueSize);
+
+    if(useExact)
+    {
+      syncExact = new message_filters::Synchronizer<ExactSyncPolicy>(ExactSyncPolicy(queueSize), *subImageColor, *subImageDepth, *subCameraInfoColor, *subCameraInfoDepth);
+      syncExact->registerCallback(boost::bind(&Receiver::callback, this, _1, _2, _3, _4));
+    }
+    else
+    {
+      syncApproximate = new message_filters::Synchronizer<ApproximateSyncPolicy>(ApproximateSyncPolicy(queueSize), *subImageColor, *subImageDepth, *subCameraInfoColor, *subCameraInfoDepth);
+      syncApproximate->registerCallback(boost::bind(&Receiver::callback, this, _1, _2, _3, _4));
+    }
+
+    spinner.start();
+
+    std::chrono::milliseconds duration(1);
+    while(!updateImage)
+    {
+      if(!ros::ok())
+      {
+        return;
+      }
+      std::this_thread::sleep_for(duration);
+    }
+    createLookup(this->color.cols, this->color.rows);
+
+    imageViewer();
+  }
+
+  void stop()
+  {
+    spinner.stop();
+
+    if(useExact)
+    {
+      delete syncExact;
+    }
+    else
+    {
+      delete syncApproximate;
+    }
+
+    delete subImageColor;
+    delete subImageDepth;
+    delete subCameraInfoColor;
+    delete subCameraInfoDepth;
+
+    pxDepthPub.shutdown();
+
+    running = false;
+  }
+
+  void callback(const sensor_msgs::Image::ConstPtr imageColor, const sensor_msgs::Image::ConstPtr imageDepth,
+                const sensor_msgs::CameraInfo::ConstPtr cameraInfoColor, const sensor_msgs::CameraInfo::ConstPtr cameraInfoDepth)
+  {
+    cv::Mat color, depth;
+
+    readCameraInfo(cameraInfoColor, cameraMatrixColor);
+    readCameraInfo(cameraInfoDepth, cameraMatrixDepth);
+    readImage(imageColor, color);
+    readImage(imageDepth, depth);
+
+    // IR image input
+    if(color.type() == CV_16U)
+    {
+      cv::Mat tmp;
+      color.convertTo(tmp, CV_8U, 0.02);
+      cv::cvtColor(tmp, color, CV_GRAY2BGR);
+    }
+
+    lock.lock();
+    this->color = color;
+    this->depth = depth;
+    updateImage = true;
+    lock.unlock();
+  }
+
+  void imageViewer()
+  {
+    cv::Mat color, depth, depthDisp, combined;
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, now;
+    double fps = 0;
+    size_t frameCount = 0;
+    std::ostringstream oss;
+    const cv::Point pos(5, 15);
+    const cv::Scalar colorText = CV_RGB(255, 255, 255);
+    const double sizeText = 0.5;
+    const int lineText = 1;
+    const int font = cv::FONT_HERSHEY_SIMPLEX;
+
+    cv::namedWindow("Image Viewer");
+    //set the callback function for any mouse event
+    cv::setMouseCallback("Image Viewer", Receiver::mouseCallbackStatic, this);
+    oss << "starting...";
+    std_msgs::UInt16 msg;
+
+    start = std::chrono::high_resolution_clock::now();
+    for(; running && ros::ok();)
+    {
+      if(updateImage)
+      {
+        lock.lock();
+        color = this->color;
+        depth = this->depth;
+        updateImage = false;
+        lock.unlock();
+
+        ++frameCount;
+        now = std::chrono::high_resolution_clock::now();
+        double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - start).count() / 1000.0;
+        if(elapsed >= 1.0)
+        {
+          fps = frameCount / elapsed;
+          oss.str("");
+          oss << "fps: " << fps << " ( " << elapsed / frameCount * 1000.0 << " ms)";
+          start = now;
+          frameCount = 0;
+        }
+
+        dispDepth(depth, depthDisp, 12000.0f);
+        combine(color, depthDisp, combined);
+        //combined = color;
+
+        cv::putText(combined, oss.str(), pos, font, sizeText, colorText, lineText, CV_AA);
+        cv::imshow("Image Viewer", combined);
+
+        if(pxDepthPub.getNumSubscribers() > 0)
+        {
+          const uint16_t *itD = depth.ptr<uint16_t>(pxLoc.y);
+          itD += pxLoc.x;
+          msg.data = *itD;
+          pxDepthPub.publish(msg);
+        }
+      }
+
+      int key = cv::waitKey(1);
+      switch(key & 0xFF)
+      {
+      case 27:
+      case 'q':
+        running = false;
+        break;
+      case ' ':
+      case 's':
+        saveImages(color, depth, depthDisp);
+        break;
+      }
+    }
+    cv::destroyAllWindows();
+    cv::waitKey(100);
+  }
+
+  static void mouseCallbackStatic( int event, int x, int y, int flags, void* that )
+  {
+      Receiver* recv = reinterpret_cast<Receiver*>(that);
+      recv->mouseCallback(event, x, y, flags);
+  }
+
+  void mouseCallback(int event, int x, int y, int flags)
+  {
+       if  ( event == cv::EVENT_RBUTTONDOWN )
+       {
+           pxLoc.x = x; pxLoc.y = y;
+           std::cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << std::endl;
+       }
+  }
+
+  void readImage(const sensor_msgs::Image::ConstPtr msgImage, cv::Mat &image) const
+  {
+    cv_bridge::CvImageConstPtr pCvImage;
+    pCvImage = cv_bridge::toCvShare(msgImage, msgImage->encoding);
+    pCvImage->image.copyTo(image);
+  }
+
+  void readCameraInfo(const sensor_msgs::CameraInfo::ConstPtr cameraInfo, cv::Mat &cameraMatrix) const
+  {
+    double *itC = cameraMatrix.ptr<double>(0, 0);
+    for(size_t i = 0; i < 9; ++i, ++itC)
+    {
+      *itC = cameraInfo->K[i];
+    }
+  }
+
+  void dispDepth(const cv::Mat &in, cv::Mat &out, const float maxValue)
+  {
+    cv::Mat tmp = cv::Mat(in.rows, in.cols, CV_8U);
+    const uint32_t maxInt = 255;
+
+    #pragma omp parallel for
+    for(int r = 0; r < in.rows; ++r)
+    {
+      const uint16_t *itI = in.ptr<uint16_t>(r);
+      uint8_t *itO = tmp.ptr<uint8_t>(r);
+
+      for(int c = 0; c < in.cols; ++c, ++itI, ++itO)
+      {
+        *itO = (uint8_t)std::min((*itI * maxInt / maxValue), 255.0f);
+      }
+    }
+
+    cv::applyColorMap(tmp, out, cv::COLORMAP_JET);
+  }
+
+  void combine(const cv::Mat &inC, const cv::Mat &inD, cv::Mat &out)
+  {
+    out = cv::Mat(inC.rows, inC.cols, CV_8UC3);
+
+    #pragma omp parallel for
+    for(int r = 0; r < inC.rows; ++r)
+    {
+      const cv::Vec3b
+      *itC = inC.ptr<cv::Vec3b>(r),
+       *itD = inD.ptr<cv::Vec3b>(r);
+      cv::Vec3b *itO = out.ptr<cv::Vec3b>(r);
+
+      for(int c = 0; c < inC.cols; ++c, ++itC, ++itD, ++itO)
+      {
+        itO->val[0] = (itC->val[0] + itD->val[0]) >> 1;
+        itO->val[1] = (itC->val[1] + itD->val[1]) >> 1;
+        itO->val[2] = (itC->val[2] + itD->val[2]) >> 1;
+      }
+    }
+  }
+
+  void saveImages(const cv::Mat &color, const cv::Mat &depth, const cv::Mat &depthColored)
+  {
+    oss.str("");
+    oss << "./" << std::setfill('0') << std::setw(4) << frame;
+    const std::string baseName = oss.str();
+    const std::string colorName = baseName + "_color.jpg";
+    const std::string depthName = baseName + "_depth.png";
+    const std::string depthColoredName = baseName + "_depth_colored.png";
+
+    OUT_INFO("saving color: " << colorName);
+    cv::imwrite(colorName, color, params);
+    OUT_INFO("saving depth: " << depthName);
+    cv::imwrite(depthName, depth, params);
+    OUT_INFO("saving depth: " << depthColoredName);
+    cv::imwrite(depthColoredName, depthColored, params);
+    OUT_INFO("saving complete!");
+    ++frame;
+  }
+
+  void createLookup(size_t width, size_t height)
+  {
+    const float fx = 1.0f / cameraMatrixColor.at<double>(0, 0);
+    const float fy = 1.0f / cameraMatrixColor.at<double>(1, 1);
+    const float cx = cameraMatrixColor.at<double>(0, 2);
+    const float cy = cameraMatrixColor.at<double>(1, 2);
+    float *it;
+
+    lookupY = cv::Mat(1, height, CV_32F);
+    it = lookupY.ptr<float>();
+    for(size_t r = 0; r < height; ++r, ++it)
+    {
+      *it = (r - cy) * fy;
+    }
+
+    lookupX = cv::Mat(1, width, CV_32F);
+    it = lookupX.ptr<float>();
+    for(size_t c = 0; c < width; ++c, ++it)
+    {
+      *it = (c - cx) * fx;
+    }
+  }
+};
+
+void help(const std::string &path)
+{
+  std::cout << path << FG_BLUE " [options]" << std::endl
+            << FG_GREEN "  name" NO_COLOR ": " FG_YELLOW "'any string'" NO_COLOR " equals to the kinect2_bridge topic base name" << std::endl
+            << FG_GREEN "  mode" NO_COLOR ": " FG_YELLOW "'qhd'" NO_COLOR ", " FG_YELLOW "'hd'" NO_COLOR ", " FG_YELLOW "'sd'" NO_COLOR " or " FG_YELLOW "'ir'" << std::endl
+            << FG_GREEN "  visualization" NO_COLOR ": " FG_YELLOW "'image'" << std::endl
+            << FG_GREEN "  options" NO_COLOR ":" << std::endl
+            << FG_YELLOW "    'compressed'" NO_COLOR " use compressed instead of raw topics" << std::endl
+            << FG_YELLOW "    'approx'" NO_COLOR " use approximate time synchronization" << std::endl;
+}
+
+int main(int argc, char **argv)
+{
+#if EXTENDED_OUTPUT
+  ROSCONSOLE_AUTOINIT;
+  if(!getenv("ROSCONSOLE_FORMAT"))
+  {
+    ros::console::g_formatter.tokens_.clear();
+    ros::console::g_formatter.init("[${severity}] ${message}");
+  }
+#endif
+
+  ros::init(argc, argv, "kinect2_plotpx", ros::init_options::AnonymousName);
+
+  if(!ros::ok())
+  {
+    return 0;
+  }
+
+  std::string ns = K2_DEFAULT_NS;
+  std::string topicColor = K2_TOPIC_QHD K2_TOPIC_IMAGE_COLOR K2_TOPIC_IMAGE_RECT;
+  std::string topicDepth = K2_TOPIC_QHD K2_TOPIC_IMAGE_DEPTH K2_TOPIC_IMAGE_RECT;
+  std::string topicPxDepth = "/pxDepth";
+  bool useExact = true;
+  bool useCompressed = false;
+
+  for(size_t i = 1; i < (size_t)argc; ++i)
+  {
+    std::string param(argv[i]);
+
+    if(param == "-h" || param == "--help" || param == "-?" || param == "--?")
+    {
+      help(argv[0]);
+      ros::shutdown();
+      return 0;
+    }
+    else if(param == "qhd")
+    {
+      topicColor = K2_TOPIC_QHD K2_TOPIC_IMAGE_COLOR K2_TOPIC_IMAGE_RECT;
+      topicDepth = K2_TOPIC_QHD K2_TOPIC_IMAGE_DEPTH K2_TOPIC_IMAGE_RECT;
+    }
+    else if(param == "hd")
+    {
+      topicColor = K2_TOPIC_HD K2_TOPIC_IMAGE_COLOR K2_TOPIC_IMAGE_RECT;
+      topicDepth = K2_TOPIC_HD K2_TOPIC_IMAGE_DEPTH K2_TOPIC_IMAGE_RECT;
+    }
+    else if(param == "ir")
+    {
+      topicColor = K2_TOPIC_SD K2_TOPIC_IMAGE_IR K2_TOPIC_IMAGE_RECT;
+      topicDepth = K2_TOPIC_SD K2_TOPIC_IMAGE_DEPTH K2_TOPIC_IMAGE_RECT;
+    }
+    else if(param == "sd")
+    {
+      topicColor = K2_TOPIC_SD K2_TOPIC_IMAGE_COLOR K2_TOPIC_IMAGE_RECT;
+      topicDepth = K2_TOPIC_SD K2_TOPIC_IMAGE_DEPTH K2_TOPIC_IMAGE_RECT;
+    }
+    else if(param == "approx")
+    {
+      useExact = false;
+    }
+
+    else if(param == "compressed")
+    {
+      useCompressed = true;
+    }
+    else
+    {
+      ns = param;
+    }
+  }
+
+  topicColor = "/" + ns + topicColor;
+  topicDepth = "/" + ns + topicDepth;
+  topicPxDepth = "/" + ns + topicPxDepth;
+  OUT_INFO("topic color: " FG_CYAN << topicColor << NO_COLOR);
+  OUT_INFO("topic depth: " FG_CYAN << topicDepth << NO_COLOR);
+
+  Receiver receiver(topicColor, topicDepth, topicPxDepth, useExact, useCompressed);
+
+  OUT_INFO("starting receiver...");
+  receiver.run();
+
+  ros::shutdown();
+  return 0;
+}
